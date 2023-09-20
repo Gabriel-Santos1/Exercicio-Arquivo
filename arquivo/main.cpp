d142ea4f7bcbe9bb862a3e0f339a0687a675a175
Gabriel-Santos1 <gabrielpaulino538@gmail.com>
2023-09-20 17:25:54 -0300

    Trabalho pronto

--- arquivo/main.cpp
+++ arquivo/main.cpp
@@ -4,6 +4,47 @@
 
 using namespace std;
 
+void menu(double medSal, double medFil, double maior, int cont)
+{
+
+    int op;
+    while (op != 0)
+    {
+        
+        system("cls");
+        cout << ("Digite 1 para Média de sálario: ") << endl;
+        cout << ("Digite 2 para Média dos filhos: ") << endl;
+        cout << ("Digite 3 para O maior sálario : ") << endl;
+        cout << ("Digite 4 para Percentual      : ") << endl;
+        cout << ("Digite 0 para Sair       ") << endl;
+        cin >> op;
+
+        switch (op)
+        {
+        case 1:
+            cout << ("Média de sálario: ") << medSal << endl;
+            break;
+
+        case 2:
+            cout << ("Média de Filhos: ") << medFil << endl;
+            break;
+
+        case 3:
+            cout << ("O maior sálario : ") << maior << endl;
+            break;
+
+        case 4:
+            cout << ("Percentual      : ") << cont << endl;
+            break;
+
+        default:
+            cout << ("Saindo...") << endl;
+            break;
+        }
+        system("pause");
+    }
+}
+
 struct banco
 {
     string nome;
@@ -22,6 +63,8 @@ int main()
     banco b[10];
     double medSal = 0, medFil = 0, maior = 0;
     int cont = 0;
+
+    // Responsável por criar o arquivo banco.txt e salvar as informações lá dentro.
 
     // ofstream arquivo("banco.txt");
 
@@ -51,6 +94,8 @@ int main()
 
     // arquivo.close();
 
+    // Reponsável por pegar o arquivo pronto e colocar na struct
+
     ifstream arquivo("banco.txt");
 
     for (int i = 0; i < 10; i++)
@@ -79,17 +124,14 @@ int main()
             cont++;
         }
     }
+
+    // Responsável por fazer as operações que o trabalho pede
+
     medSal /= 10;
     medFil /= 10;
-    cont = (cont/5)*100;
+    cont = (cont / 5) * 100;
 
-
-
-    cout << ("Média de sálario: ") << medSal << endl;
-    cout << ("Média dos filhos: ") << medFil << endl;
-    cout << ("O maior sálario : ") << maior << endl;
-    cout << ("Percentual      : ") << cont << endl;
-
+    menu(medSal,medFil,maior,cont);
 
     cout << endl
          << endl;
